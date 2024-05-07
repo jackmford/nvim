4936a9f634e421ddc4f699080f9e2f166306ca63
jackmford <jackmitchellfordyce@gmail.com>
2024-05-07 17:00:39 -0500

    initial

---
+++ init.lua
@@ -0,0 +1,14 @@
+require("plugins")
+require("whichkey")
+require("options")
+require("telescope")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
